09655b6bd0fb80499e63c005755e14c4332e0643
HEETVICHHIVORA <heetvic@gmail.com>
2024-04-29 02:52:25 +0530

    com

---
+++ merge.cpp
@@ -0,0 +1,67 @@
+#include<iostream> 
+#include<vector>
+using namespace std ;
+void merge(vector<int>& arr,int s ,int e){
+    int mid = (s+e)/2;
+
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    //copy values
+    int mainArrayIndex = s;
+    for(int i=0; i<len1; i++) {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid+1;
+    for(int i=0; i<len2; i++) {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    //merge 2 sorted arrays     
+    int index1 = 0;
+    int index2 = 0;
+    mainArrayIndex = s;
+
+    while(index1 < len1 && index2 < len2) {
+        if(first[index1] < second[index2]) {
+            arr[mainArrayIndex++] = first[index1++];
+        }
+        else{
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }   
+
+    while(index1 < len1) {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while(index2 < len2 ) {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+
+    delete []first;
+    delete []second;
+}
+void mergesort(vector<int> &arr,int s,int e){
+    int mid =(s+e)/2;
+    if(s>=e){
+        return ;
+    }  
+    mergesort(arr,s,mid);
+    mergesort(arr,mid+1,e);
+    merge(arr,s,e);
+
+}
+int main (){
+    vector<int>arr={10,9,8,7,6,5,4,3,2,1};
+    int n = arr.size();
+    mergesort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "<<endl;
+    }
+    return 0 ;
+}
